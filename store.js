0fea2e319b97086c348f4c4177a377c449ed8b24
Mithunmsrd <mithunanayadi@gmail.com>
2024-06-12 23:25:25 +0530

    first commit

---
+++ store.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const log = ((req, res, next) =>{
+  console.log(`[${new Date().toISOString()} ${req.method} ${req.url}]`);
+  next();
+});
+
+app.use(log);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
